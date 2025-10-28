b83ac31442d69926b66dc0a74872c05aa9464d0e
bernad <bernadinusnaisau230803@gmail.com>
2025-10-28 22:33:18 +0700

    add entity user for representate table users in database

---
+++ src/main/java/expense/tracker/entity/User.java
@@ -0,0 +1,28 @@
+package expense.tracker.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String password;
+    private String username;
+    private String token;
+
+    @OneToMany(mappedBy = "user")
+    private List<Expense> expense;
+}
