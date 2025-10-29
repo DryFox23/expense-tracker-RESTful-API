0b7b43c2e5e8eb81d6812214f88c1d65da142508
bernad <bernadinusnaisau230803@gmail.com>
2025-10-29 20:55:12 +0700

    add UserRepository for query operation to database

---
+++ src/main/java/expense/tracker/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package expense.tracker.repository;
+
+import expense.tracker.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
