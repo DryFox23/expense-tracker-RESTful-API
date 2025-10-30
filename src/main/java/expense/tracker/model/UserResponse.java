a4f57429f536653030d5d7f6e64dd40c10402b08
bernad <bernadinusnaisau230803@gmail.com>
2025-10-30 20:39:54 +0700

    add UserResponse for response any request from user

---
+++ src/main/java/expense/tracker/model/UserResponse.java
@@ -0,0 +1,17 @@
+package expense.tracker.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserResponse {
+
+    private String email;
+    private String username;
+    private String message;
+}
