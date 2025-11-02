977891cb09a8f441218d4cf9ce8a91b831565870
bernad <bernadinusnaisau230803@gmail.com>
2025-11-02 19:07:45 +0700

    change query method findFirstBy to boolean existsBy

--- src/main/java/expense/tracker/repository/UserRepository.java
+++ src/main/java/expense/tracker/repository/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    Optional<User> findFirstByEmail(String email);
+    boolean existsByEmail(String email);
 }
