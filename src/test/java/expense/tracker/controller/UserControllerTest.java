394cc2f58e52a3aa8b0dea6dc8e8d2fa44558df9
bernad <bernadinusnaisau230803@gmail.com>
2025-11-05 23:00:00 +0700

    add unit test for get user data and update data user using mock test with case failed and success

--- src/test/java/expense/tracker/controller/UserControllerTest.java
+++ src/test/java/expense/tracker/controller/UserControllerTest.java
@@ -4,19 +4,21 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import expense.tracker.entity.User;
 import expense.tracker.model.RegisterUserRequest;
+import expense.tracker.model.UpdateUserRequest;
+import expense.tracker.model.UserResponse;
 import expense.tracker.model.WebResponse;
 import expense.tracker.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
+import expense.tracker.service.JwtService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
@@ -34,6 +36,12 @@ public class UserControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    private JwtService jwtService;
+
+    private String token;
+    private String authHeader;
 
     @BeforeEach
     void setUp() {
@@ -87,6 +95,97 @@ public class UserControllerTest {
 
                     assertNotNull(response.getErrors());
                     System.out.println(response.getErrors());
+                });
+    }
+
+    @Test
+    void getUserInfoSuccess() throws Exception {
+        User user = new User();
+        user.setEmail("email@example.com");
+        user.setUsername("username");
+        user.setPassword("password");
+        userRepository.save(user);
+
+        token = jwtService.generateToken(user.getId(), user.getEmail());
+        authHeader = "Bearer " + token;
+
+        mockMvc.perform(get("/api/v1/users/current")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", authHeader))
+                .andExpectAll(status().isOk())
+                .andDo(result -> {
+                    WebResponse<UserResponse> response = objectMapper.readValue(result.getResponse().getContentAsString(),
+                            new TypeReference<WebResponse<UserResponse>>() {
+                            });
+
+                    assertNull(response.getErrors());
+                    assertEquals(response.getData().getEmail(), user.getEmail());
+                    assertEquals(response.getData().getUsername(), user.getUsername());
+                    System.out.println(response.getMessage());
+                });
+    }
+
+    @Test
+    void getUserInfoFailed() throws Exception {
+
+        User user = new User();
+        user.setEmail("email@example.com");
+        user.setUsername("username");
+        user.setPassword("password");
+        userRepository.save(user);
+
+        token = jwtService.generateToken(user.getId(), user.getEmail());
+        authHeader = "Bearerr " + token;
+
+        mockMvc.perform(get("/api/v1/users/current")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", authHeader))
+                .andExpectAll(status().isUnauthorized())
+                .andDo(result -> {
+                    WebResponse<UserResponse> response = objectMapper.readValue(result.getResponse().getContentAsString(),
+                            new TypeReference<WebResponse<UserResponse>>() {
+                            });
+
+                    assertNotNull(response.getErrors());
+                    System.out.println(response.getErrors());
+                });
+    }
+
+    @Test
+    void updateDataUserSuccess() throws Exception {
+        User user = new User();
+        user.setEmail("email@example.com");
+        user.setPassword(BCrypt.hashpw("password", BCrypt.gensalt()));
+        user.setUsername("username");
+        userRepository.save(user);
+
+        token = jwtService.generateToken(user.getId(), user.getEmail());
+        authHeader = "Bearer " + token;
+
+        UpdateUserRequest request = new UpdateUserRequest();
+        request.setEmail("update@example.com");
+        request.setPassword("update");
+        request.setUsername("username");
+
+        mockMvc.perform(patch("/api/v1/users/" + user.getId())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+                .header("Authorization", authHeader))
+                .andExpectAll(status().isOk())
+                .andDo(result -> {
+                    WebResponse<UserResponse> response = objectMapper.readValue(result.getResponse().getContentAsString(),
+                            new TypeReference<WebResponse<UserResponse>>() {
+                            });
+                    assertNull(response.getErrors());
+                    assertNotEquals(response.getData().getEmail(), user.getEmail());
+                    assertEquals(response.getData().getUsername(), user.getUsername());
+
+                    User userDb = userRepository.findByEmail("update@example.com").orElse(null);
+                    assertTrue(BCrypt.checkpw("update", userDb.getPassword()));
+                    System.out.println(response.getMessage());
                 });
     }
 }
