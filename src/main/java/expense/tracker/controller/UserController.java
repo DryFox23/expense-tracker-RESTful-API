e9a2d7887587f7bbb031e43f1dc78c87ffd18dda
bernad <bernadinusnaisau230803@gmail.com>
2025-11-05 23:01:11 +0700

    change url in registerUser and add new controller for getDataUser, updateDataUser

--- src/main/java/expense/tracker/controller/UserController.java
+++ src/main/java/expense/tracker/controller/UserController.java
@@ -1,13 +1,14 @@
 package expense.tracker.controller;
 
+import expense.tracker.entity.User;
 import expense.tracker.model.RegisterUserRequest;
+import expense.tracker.model.UpdateUserRequest;
+import expense.tracker.model.UserResponse;
 import expense.tracker.model.WebResponse;
 import expense.tracker.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -15,7 +16,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping(path = "api/v1/user",
+    @PostMapping(path = "/api/v1/users",
     consumes = MediaType.APPLICATION_JSON_VALUE,
     produces = MediaType.APPLICATION_JSON_VALUE)
     public WebResponse<String> registerUser(@RequestBody RegisterUserRequest request){
@@ -23,6 +24,27 @@ public class UserController {
         return WebResponse.<String>builder()
                 .message("Success register user")
                 .data(null)
+                .build();
+    }
+
+    @GetMapping(path = "/api/v1/users/current")
+    public WebResponse<UserResponse> getDataUser(User user){
+        UserResponse response = userService.getUser(user);
+        return WebResponse.<UserResponse>builder()
+                .message("User Info")
+                .data(response)
+                .build();
+    }
+
+    @PatchMapping(path = "/api/v1/users/{userId}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponse<UserResponse> updateDataUser(@PathVariable String userId
+                                                    ,@RequestBody UpdateUserRequest request){
+        UserResponse response = userService.updateUser(userId, request);
+        return WebResponse.<UserResponse>builder()
+                .message("Update Data User")
+                .data(response)
                 .build();
     }
 }
