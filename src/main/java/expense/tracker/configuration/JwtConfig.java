39ca420a6c9dedbe65faa47015bd4cce5b67f913
bernad <bernadinusnaisau230803@gmail.com>
2025-11-05 20:03:34 +0700

    add annotation @ConfigurationProperties for catch application properties jwt field

--- src/main/java/expense/tracker/configuration/JwtConfig.java
+++ src/main/java/expense/tracker/configuration/JwtConfig.java
@@ -1,17 +1,14 @@
 package expense.tracker.configuration;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Component
 @Data
+@Component
+@ConfigurationProperties(prefix = "jwt")
 public class JwtConfig {
 
-    @Value("${jwt.secret}")
     private String secretKey;
-
-    @Value("${jwt.expiration}")
     private long expiration;
 }
