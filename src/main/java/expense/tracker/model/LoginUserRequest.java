fa0cd7597214c01e3e32c1d4ed4bc1f68a679589
bernad <bernadinusnaisau230803@gmail.com>
2025-11-05 20:08:51 +0700

    add LoginUserRequest for requesting field mandatory at Login method

---
+++ src/main/java/expense/tracker/model/LoginUserRequest.java
@@ -0,0 +1,26 @@
+package expense.tracker.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginUserRequest {
+
+    @Email
+    private String email;
+    @NotBlank
+    @Size(min =6, max = 100)
+    private String password;
+
+    @NotBlank
+    @Size(min =6, max = 100)
+    private String username;
+}
