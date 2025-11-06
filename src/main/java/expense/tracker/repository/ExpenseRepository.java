f307656934b49ac1ee247b13202c4133a9525f79
bernad <bernadinusnaisau230803@gmail.com>
2025-11-06 22:04:55 +0700

    add query method for get single data from db

--- src/main/java/expense/tracker/repository/ExpenseRepository.java
+++ src/main/java/expense/tracker/repository/ExpenseRepository.java
@@ -1,9 +1,14 @@
 package expense.tracker.repository;
 
 import expense.tracker.entity.Expense;
+import expense.tracker.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, String> {
+
+    Optional<Expense> findFirstByUserAndId(User user, String expenseId);
 }
