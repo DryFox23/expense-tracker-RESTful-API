1026a14a0204eb45729378d52d20ff1e60a2c56a
bernad <bernadinusnaisau230803@gmail.com>
2025-11-06 22:07:46 +0700

    add UpdateExpenseRequest for requesting to update expense data

---
+++ src/main/java/expense/tracker/model/UpdateExpenseRequest.java
@@ -0,0 +1,30 @@
+package expense.tracker.model;
+
+import expense.tracker.entity.ExpenseCategory;
+import expense.tracker.entity.ExpensePaymentMethod;
+import expense.tracker.entity.ExpenseType;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateExpenseRequest {
+
+    private String id;
+    @NotNull
+    private String title;
+    private BigDecimal amount;
+    private String description;
+    private LocalDate date;
+    private ExpenseType type;
+    private ExpenseCategory category;
+    private ExpensePaymentMethod paymentMethod;
+}
