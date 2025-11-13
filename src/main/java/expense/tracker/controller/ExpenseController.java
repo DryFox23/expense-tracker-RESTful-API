d06b2f722f4a45e7d8eb8a0a97d91f4c6116f400
bernad <bernadinusnaisau230803@gmail.com>
2025-11-13 19:05:55 +0700

    add new controller for search all expense

--- src/main/java/expense/tracker/controller/ExpenseController.java
+++ src/main/java/expense/tracker/controller/ExpenseController.java
@@ -1,14 +1,18 @@
 package expense.tracker.controller;
 
+import expense.tracker.entity.ExpenseCategory;
+import expense.tracker.entity.ExpenseType;
 import expense.tracker.entity.User;
-import expense.tracker.model.CreateExpenseRequest;
-import expense.tracker.model.ExpenseResponse;
-import expense.tracker.model.UpdateExpenseRequest;
-import expense.tracker.model.WebResponse;
+import expense.tracker.model.*;
 import expense.tracker.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 public class ExpenseController {
@@ -57,6 +61,38 @@ public class ExpenseController {
         return WebResponse.<String>builder()
                 .message("delete expense")
                 .data("success delete expense")
+                .build();
+    }
+
+    @GetMapping(path = "/api/v1/expenses",
+    produces = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponse<List<ExpenseResponse>> searchExpense(User user,
+                                                            @RequestParam (value = "title", required = false) String title,
+                                                            @RequestParam (value = "amount", required = false)BigDecimal amount,
+                                                            @RequestParam (value = "type", required = false)ExpenseType type,
+                                                            @RequestParam (value = "category", required = false) ExpenseCategory category,
+                                                            @RequestParam (value = "pageNumber", required = false, defaultValue = "0") Integer pageNumber,
+                                                            @RequestParam (value = "pageSize", required = false, defaultValue = "10") Integer pageSize){
+
+        SearchExpenseRequest request = SearchExpenseRequest.builder()
+                .title(title)
+                .amount(amount)
+                .category(category)
+                .expenseType(type)
+                .pageNumber(pageNumber)
+                .pageSize(pageSize)
+                .build();
+
+        Page<ExpenseResponse> expensePage = expenseService.searchExpense(user, request);
+        return WebResponse.<List<ExpenseResponse>>builder()
+                .message("search expense")
+                .errors("expense not found")
+                .data(expensePage.getContent())
+                .pagingResponse(PagingResponse.builder()
+                        .currentPage(expensePage.getNumber())
+                        .totalPages(expensePage.getTotalPages())
+                        .totalSize(expensePage.getSize())
+                        .build())
                 .build();
     }
 }
