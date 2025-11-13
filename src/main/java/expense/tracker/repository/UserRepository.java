43d4a2779a9aef1dc6ce80792062e06758a87a21
bernad <bernadinusnaisau230803@gmail.com>
2025-11-13 19:13:18 +0700

    add query method for delete token

--- src/main/java/expense/tracker/repository/UserRepository.java
+++ src/main/java/expense/tracker/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package expense.tracker.repository;
 
 import expense.tracker.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +15,9 @@ public interface UserRepository extends JpaRepository<User, String> {
     boolean existsByEmail(String email);
 
     Optional<User> findByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query("update User u set u.token = null, u.token = null where u.token < :now")
+    int deleteExpiredTokens(@Param("now") Long now);
 }
