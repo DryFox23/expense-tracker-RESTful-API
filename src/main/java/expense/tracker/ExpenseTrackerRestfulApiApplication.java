6ddcd4a530bcb9919f983cb3037096fe844f552c
bernad <bernadinusnaisau230803@gmail.com>
2025-11-13 19:15:52 +0700

    add annotation @EnableScheduling for enable schedul

--- src/main/java/expense/tracker/ExpenseTrackerRestfulApiApplication.java
+++ src/main/java/expense/tracker/ExpenseTrackerRestfulApiApplication.java
@@ -4,9 +4,11 @@ import expense.tracker.configuration.JwtConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties(JwtConfig.class)
+@EnableScheduling
 public class ExpenseTrackerRestfulApiApplication {
 
 	public static void main(String[] args) {
