f097f8716991b9beee18f915c55fcb40cbb0699f
bernad <bernadinusnaisau230803@gmail.com>
2025-11-13 19:09:17 +0700

    add JpaSpecificationExecutor for supporting query search all expense

--- src/main/java/expense/tracker/repository/ExpenseRepository.java
+++ src/main/java/expense/tracker/repository/ExpenseRepository.java
@@ -3,12 +3,13 @@ package expense.tracker.repository;
 import expense.tracker.entity.Expense;
 import expense.tracker.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ExpenseRepository extends JpaRepository<Expense, String> {
+public interface ExpenseRepository extends JpaRepository<Expense, String>, JpaSpecificationExecutor<Expense> {
 
     Optional<Expense> findFirstByUserAndId(User user, String expenseId);
 }
